24291b38857cbd7a2087de5b6aea9c96889ad246
kaylen ✨🍓 <actuallyori@gmail.com>
2024-06-10 11:44:06 +0100

    feat: declare zsh globally

--- home/packages.nix
+++ home/packages.nix
@@ -1,27 +1,7 @@
 {pkgs, inputs, ...}: {
   nixpkgs.config.allowUnfree = false;
-  home.packages = with pkgs; [
-    vim
-    zsh
-  ];
+  home.packages = with pkgs; [];
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      vim = "nvim";
-    };
-
-    plugins = [
-      { name = "marlonrichert/zsh-autocomplete"; }
-      { name = "zsh-users/zsh-syntax-highlighting"; }
-      { name = "zsh-users/zsh-completions"; }
-    ];
-  };
-  
   # https://starship.rs/installing/#nix
   programs.starship.enable = true;
 }
--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -7,4 +7,26 @@
     curl
     btrfs-progs
   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      vim = "nvim";
+    };
+
+    plugins = [
+      { name = "marlonrichert/zsh-autocomplete"; }
+      { name = "zsh-users/zsh-syntax-highlighting"; }
+      { name = "zsh-users/zsh-completions"; }
+    ];
+  };
 }
