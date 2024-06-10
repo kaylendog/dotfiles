38e85a8a840bcce9af54bff75a1ea02fbc209314
kaylen ✨🍓 <actuallyori@gmail.com>
2024-06-10 11:45:22 +0100

    fix: typo in autosuggestions

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -16,7 +16,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestion.enable = true;
+    autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
 
     shellAliases = {
